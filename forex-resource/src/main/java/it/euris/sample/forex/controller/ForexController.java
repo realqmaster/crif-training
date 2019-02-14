e934ef7f4060bcf37b56f9e3d957cfc250557482
michaelcauduro92 <44635342+michaelcauduro92@users.noreply.github.com>
2019-02-14 09:52:03 +0100

    commit Michael

--- forex-resource/src/main/java/it/euris/sample/forex/controller/ForexController.java
+++ forex-resource/src/main/java/it/euris/sample/forex/controller/ForexController.java
@@ -1,23 +1,41 @@
 package it.euris.sample.forex.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import it.euris.sample.forex.entity.ExchangeValue;
+import it.euris.sample.forex.error.controller.MyErrorController;
 import it.euris.sample.forex.repository.ExchangeValueRepository;
 
 @RestController
-public class ForexController {
+public class ForexController extends MyErrorController{
 
 	@Autowired
 	private ExchangeValueRepository repository;
-
+	
+	
+	@GetMapping("/currency-exchange")
+	//visualizzo tutti i cambi che ho a disposizione. path di default come "pagina principale"
+	public List<ExchangeValue> sowAllExchangeValue() {
+		
+		List<ExchangeValue> exchangeValue = repository.findAll();
+		
+		return exchangeValue;
+	}
+	
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	//visualizzo il cambio tra due valute prese in input
 	public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
 
 		ExchangeValue exchangeValue = repository.findByFromAndTo(from, to);
+		
 		return exchangeValue;
-	}
+	}	
+	
+
+	
 }
---
+++ forex-resource/src/main/java/it/euris/sample/forex/error/controller/MyErrorController.java
@@ -0,0 +1,29 @@
+package it.euris.sample.forex.error.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public  class MyErrorController implements ErrorController {
+
+  @GetMapping("/error")
+  @ResponseBody
+  //Metodo di gestione degli errori. esempio principale visto 404
+  public String handleError(HttpServletRequest request) {
+      Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+      Exception exception = (Exception) request.getAttribute("javax.servlet.error.exception");
+      return String.format("<html><body><h2>Error Page</h2><div>Status code: <b>%s</b></div>"
+                      + "<div>Exception Message: <b>%s</b></div><body></html>",
+              statusCode, exception==null? "N/A": exception.getMessage());
+	  
+  }
+
+  @Override
+  public String getErrorPath() {
+      return "/error";
+  }
+}
